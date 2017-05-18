9c210253a3baf803fd8e0faff0bdb4bd9659517f
dutingting <1605205026@qq.com>
2017-05-18 18:05:34 +0800

    修改了逻辑

--- JustJava/app/src/main/java/exampleexample/testing/justjava/MainActivity.java
+++ JustJava/app/src/main/java/exampleexample/testing/justjava/MainActivity.java
@@ -1,23 +1,51 @@
 package exampleexample.testing.justjava;
 
+import android.icu.text.NumberFormat;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void submitOrder(View view){
-            display(1888888);
+    public void addOrder(View view){
+          count=count+1;
+            display(count);
+
+    }
+    public void reduceOrder(View view){
+        count =count-1;
+        if(count<0){
+            count=0;
+        }
+        else {
+            display(count);
+        }
+
+    }
+    public void submitOrder(){
+        int price=8;
+        displayPrice(count*price);
     }
     public  void  display(int number){
-        TextView textView=(TextView)findViewById(R.id.count_text_view);
-        textView.setText(" "+number);
+      TextView textView=(TextView) findViewById(R.id.count_text_view);
+        textView.setText(""+number);
 
 }
+
+    public  void  displayPrice(int price){
+        TextView textView=(TextView) findViewById(R.id.pricecount_text_view);
+//        textView.setText(NumberFormat.getCurrencyInstance().format(price));
+            textView.setText(""+price);
+
+
+    }
 }

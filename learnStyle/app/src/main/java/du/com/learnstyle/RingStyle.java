68aca2deb07fcb16e9cdd6a79ed75dd6bbd8222a
dutingting <1605205026@qq.com>
2017-06-30 10:45:34 +0800

    layer_list

--- learnStyle/app/src/main/java/du/com/learnstyle/RingStyle.java
+++ learnStyle/app/src/main/java/du/com/learnstyle/RingStyle.java
@@ -1,7 +1,11 @@
 package du.com.learnstyle;
 
+import android.content.Intent;
+import android.icu.text.AlphabeticIndex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RingStyle extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class RingStyle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ring_style);
+        Button button=(Button) findViewById(R.id.bt_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RingStyle.this,TableStyle.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ learnStyle/app/src/main/java/du/com/learnstyle/TableStyle.java
@@ -0,0 +1,13 @@
+package du.com.learnstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableStyle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_style);
+    }
+}
